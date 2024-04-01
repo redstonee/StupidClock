107f9fd2897e27b4c5b3e88326671c38441d07e9
redstoneee <dc_redstone@outlook.com>
2024-04-01 20:30:17 +0800

    Add NTP library

--- include/Network.hpp
+++ include/Network.hpp
@@ -1,4 +1,24 @@
 #pragma once
 
-#include<WiFi.h>
-#include<Preferences.h>
+#include <WiFi.h>
+#include <Preferences.h>
+#include <WiFiUdp.h>
+#include <NTPClient.h>
+
+namespace Network
+{
+    void ntpTask(void *shit)
+    {
+
+
+        while (1)
+        {
+#ifdef DEBUGING
+            Serial.printf("ntp:");
+            Serial.println(DateTime.toISOString());
+#endif
+
+            delay(10000);
+        }
+    }
+} // namespace Network
